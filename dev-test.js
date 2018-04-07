bf4b9a4f8fb9bd8e1b8da82d296f6f7648ae3985
Hristian233 <hristian996@abv.bg>
2018-04-07 16:25:30 +0300

    Add files via upload

--- dev-test.js
+++ dev-test.js
@@ -1,7 +1,4 @@
 const Block = require('./block');
 
-const block = new Block('01/01/2018','lastHash','newHash','info');
-
-console.log(block.toString());
-
-console.log(Block.genesis().toString());
+const myBlock = Block.mineBlock(Block.genesis(),'info');
+console.log(myBlock.toString());
